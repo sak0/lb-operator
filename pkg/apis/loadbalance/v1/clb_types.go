a9db96aab98c5bf0fe6ec515a6249e68bdad74e8
CuiHaozhi <61755280@qq.com>
2018-07-18 23:06:08 +0800

    add clb..
    Signed-off-by: CuiHaozhi <61755280@qq.com>

---
+++ pkg/apis/loadbalance/v1/clb_types.go
@@ -0,0 +1,35 @@
+package v1
+
+import (
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// Definition of our CRD AppLoadBalance class
+type ClassicLoadBalance struct {
+	meta_v1.TypeMeta   `json:",inline"`
+	meta_v1.ObjectMeta `json:"metadata"`
+	Spec               ClassicLoadBalanceSpec   `json:"spec"`
+	Status             ClassicLoadBalanceStatus `json:"status,omitempty"`
+}
+
+type ClassicLoadBalanceSpec struct {
+	IP		string						`json:"ip,omitempty"`
+	Port	string						`json:"port,omitempty"`
+	Backend	ClassicLoadBalanceBackend	`json:"backend,omitempty"`
+}
+
+type ClassicLoadBalanceBackend struct {
+	ServiceName	string	`json:"serviceName"`
+	ServicePort	int		`json:"servicePort"`
+}
+
+type ClassicLoadBalanceStatus struct {
+	State   string `json:"state,omitempty"`
+	Message string `json:"message,omitempty"`
+}
+
+type ClassicLoadBalanceList struct {
+	meta_v1.TypeMeta `json:",inline"`
+	meta_v1.ListMeta `json:"metadata"`
+	Items            []ClassicLoadBalance `json:"items"`
+}
