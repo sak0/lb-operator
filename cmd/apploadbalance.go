eb9ff85e6a30b464e10d9a7912e32738d083134c
CuiHaozhi <61755280@qq.com>
2018-07-22 22:26:27 +0800

    init metrics.
    Signed-off-by: CuiHaozhi <61755280@qq.com>

--- cmd/apploadbalance.go
+++ cmd/apploadbalance.go
@@ -2,20 +2,32 @@ package main
 
 import (
 	"flag"
+	"log"
+	"net"
+	"net/http"	
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
-		
+
 	"github.com/golang/glog"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 
 	"github.com/sak0/lb-operator/pkg/controller"
 	"github.com/sak0/lb-operator/pkg/utils"
+)
+
+const (
+	healthzPath = "/healthz"
 )
 
 var (
 	kubeconf	= flag.String("kubeconf", "admin.conf", "Path to a kube config. Only required if out-of-cluster.")
 	runTest		= flag.Bool("runtest", false, "If create test resource.")
 	createCrd	= flag.Bool("createCrd", true, "If create crd.")
+	
+	metricsPath	= flag.String("metrics-path", "/metrics", "metrcis url path.")
+	metricsPort	= flag.Int("port", 8080, "metrics listen port.")
 )
 
 func main() {
@@ -50,6 +62,24 @@ func main() {
 	go albctr.Run(stopCh)
 	go clbctr.Run(stopCh)
 
+
+	//Create exporter for prometheus
+	http.Handle(*metricsPath, promhttp.Handler())
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`<html>
+			<head><title>LoadBalance Controller</title></head>
+			<body>
+			<h1>Hello LB</h1>
+			<p><a href='` + *metricsPath + `'>Metrics</a></p>
+			</body>
+			</html>`))
+	})
+	http.HandleFunc(healthzPath, func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Write([]byte("ok"))
+	})
+	listenAddress := net.JoinHostPort("0.0.0.0", strconv.Itoa(*metricsPort))
+	go log.Fatal(http.ListenAndServe(listenAddress, nil))	
 
 	//Catch signal for exit.
 	c := make(chan os.Signal, 1)
