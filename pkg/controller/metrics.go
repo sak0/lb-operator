8ca353ae9621ee0dcfd72994f5c2b0100ef35716
CuiHaozhi <61755280@qq.com>
2018-07-23 23:39:03 +0800

    init metrics.
    Signed-off-by: CuiHaozhi <61755280@qq.com>

---
+++ pkg/controller/metrics.go
@@ -0,0 +1,54 @@
+package controller
+
+import "github.com/prometheus/client_golang/prometheus"
+
+var (
+	clbTotal = prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Namespace: "clb_operator",
+				Subsystem: "controller",
+				Name:      "clb_total",
+				Help:      "Total number of classic loadbalance managed by the controller",
+			},
+			[]string{"clb_name", "namespace"},
+	)
+
+	clbRequestTotal = prometheus.NewCounterVec(
+			prometheus.CounterOpts{
+				Namespace: "clb_operator",
+				Subsystem: "controller",
+				Name:      "request_total",
+				Help:      "Total request number of the clb",
+			},
+			[]string{"clb_name", "namespace"},
+	)
+
+	clbResponseTotal = prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: "clb_operator",
+		Subsystem: "controller",
+		Name:      "response_total",
+		Help:      "Total response number of the clb",
+	})
+
+	clbRequestBytesTotal = prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: "clb_operator",
+		Subsystem: "controller",
+		Name:      "request_bytes_total",
+		Help:      "Total request bytes of the clb",
+	})
+
+	clbResponseBytesTotal = prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: "clb_operator",
+		Subsystem: "controller",
+		Name:      "response_bytes_total",
+		Help:      "Total response bytes of the clb",
+	})
+)
+
+func init() {
+	prometheus.MustRegister(clbTotal)
+	prometheus.MustRegister(clbRequestTotal)
+	prometheus.MustRegister(clbResponseTotal)
+	prometheus.MustRegister(clbRequestBytesTotal)
+	prometheus.MustRegister(clbResponseBytesTotal)
+}
