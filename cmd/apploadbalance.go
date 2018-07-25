5861a76e24a27c7d904e896f321dfda1fda17b9b
CuiHaozhi <61755280@qq.com>
2018-07-25 15:20:29 +0800

    enable leaderelection.
    Signed-off-by: CuiHaozhi <61755280@qq.com>

--- cmd/apploadbalance.go
+++ cmd/apploadbalance.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"flag"
-	"log"
 	"net"
 	"net/http"	
-	"os"
-	"os/signal"
 	"strconv"
-	"syscall"
+	"time"
 
 	"github.com/golang/glog"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	"k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
+	v1core "k8s.io/client-go/kubernetes/typed/core/v1"
+
+	"k8s.io/client-go/tools/leaderelection"
+	"k8s.io/client-go/tools/leaderelection/resourcelock"
+	"k8s.io/client-go/tools/record"	
 
 	"github.com/sak0/lb-operator/pkg/controller"
 	"github.com/sak0/lb-operator/pkg/utils"
@@ -28,14 +34,16 @@ var (
 	
 	metricsPath	= flag.String("metrics-path", "/metrics", "metrcis url path.")
 	metricsPort	= flag.Int("port", 8888, "metrics listen port.")
+	
+	//TODO read from env.
+	electionName		= flag.String("name", "lb-operator", "electionName for this instance.")
+	electionId			= flag.String("id", "host123", "electionId for this instance.")
+	electionNamespace	= flag.String("namespace", "default", "election resource's Namespace.")
 )
 
-func main() {
-	flag.Parse()
-	
+func run(stopCh <-chan struct{}){
 	// Get all clients
 	kubeClient, extClient, crdcs, scheme, err := utils.CreateClients(*kubeConf)
-	
 	if err != nil {
 		panic(err.Error())
 	}
@@ -47,23 +55,26 @@ func main() {
 			panic(err.Error())
 		}
 	}
-	
-	// Create some test resources if needed
+	/*// Create some test resources if needed
 	if *runTest {
 		glog.V(2).Infof("Creating test resource...")
 		utils.RunAlbExample(crdcs, scheme)
 		utils.RunClbExample(crdcs, scheme)
-	}
+	}*/		
 
 	// Run controllers
 	// Watch for changes in AppLoadBalance and ClassicLoadBalance objects and fire Add, Delete, Update callbacks
-	stopCh := make(chan struct{})
+	//stopCh := make(chan struct{})
 	albctr, _ := controller.NewALBController(kubeClient, crdcs, scheme)
 	clbctr, _ := controller.NewCLBController(kubeClient, crdcs, scheme)
 	go albctr.Run(stopCh)
 	go clbctr.Run(stopCh)
+}
 
 
+func main() {
+	flag.Parse()	
+	
 	//Create exporter for prometheus
 	http.Handle(*metricsPath, promhttp.Handler())
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -80,14 +91,52 @@ func main() {
 		w.Write([]byte("ok"))
 	})
 	listenAddress := net.JoinHostPort("0.0.0.0", strconv.Itoa(*metricsPort))
-	go log.Fatal(http.ListenAndServe(listenAddress, nil))	
+	go http.ListenAndServe(listenAddress, nil)
 	
+	kubeclient := utils.MustNewKubeClient()
+	glog.V(2).Infof("%v", kubeclient)
 
+	rl, err := resourcelock.New(resourcelock.EndpointsResourceLock,
+		*electionNamespace,
+		"lb-operator",
+		kubeclient.Core(),
+		resourcelock.ResourceLockConfig{
+			Identity:      *electionId,
+			EventRecorder: createRecorder(kubeclient, *electionName, *electionNamespace),
+		})
+	if err != nil {
+		glog.Fatalf("error creating lock: %v", err)
+		panic(err)
+	}
+
+	leaderelection.RunOrDie(leaderelection.LeaderElectionConfig{
+		Lock:          rl,
+		LeaseDuration: 15 * time.Second,
+		RenewDeadline: 10 * time.Second,
+		RetryPeriod:   2 * time.Second,
+		Callbacks: leaderelection.LeaderCallbacks{
+			OnStartedLeading: run,
+			OnStoppedLeading: func() {
+				glog.Fatalf("leader election lost")
+			},
+		},
+	})	
+	
+	
+	/*stopCh := make(<-chan struct{})
+	run(stopCh)
 	//Catch signal for exit.
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	glog.V(2).Infof("signal.Notify ready..")
 	<-c
-	close(stopCh)
-	glog.V(2).Infof("Bye bye...")
+	//close(stopCh)
+	glog.V(2).Infof("Bye bye...")*/
+}
+
+func createRecorder(kubecli kubernetes.Interface, name, namespace string) record.EventRecorder {
+	eventBroadcaster := record.NewBroadcaster()
+	eventBroadcaster.StartLogging(glog.Infof)
+	eventBroadcaster.StartRecordingToSink(&v1core.EventSinkImpl{Interface: v1core.New(kubecli.Core().RESTClient()).Events(namespace)})
+	return eventBroadcaster.NewRecorder(scheme.Scheme, v1.EventSource{Component: name})
 }
--- pkg/utils/k8sutil.go
+++ pkg/utils/k8sutil.go
@@ -2,10 +2,14 @@ package utils
 
 import (
 	"fmt"
+	"net"
+	"os"
 	"strconv"
 	"time"	
 	//crdv1 "github.com/sak0/lb-operator/pkg/apis/loadbalance/v1"
 	"k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	
 	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
@@ -54,4 +58,32 @@ func GetEndpointMap(ep *v1.Endpoints)map[string]int{
 	}
 	
 	return ipmap
+}
+
+func InClusterConfig() (*rest.Config, error) {
+	// Work around https://github.com/kubernetes/kubernetes/issues/40973
+	// See https://github.com/coreos/etcd-operator/issues/731#issuecomment-283804819
+	if len(os.Getenv("KUBERNETES_SERVICE_HOST")) == 0 {
+		addrs, err := net.LookupHost("kubernetes.default.svc")
+		if err != nil {
+			panic(err)
+		}
+		os.Setenv("KUBERNETES_SERVICE_HOST", addrs[0])
+	}
+	if len(os.Getenv("KUBERNETES_SERVICE_PORT")) == 0 {
+		os.Setenv("KUBERNETES_SERVICE_PORT", "443")
+	}
+	cfg, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
+
+func MustNewKubeClient() kubernetes.Interface {
+	cfg, err := InClusterConfig()
+	if err != nil {
+		panic(err)
+	}
+	return kubernetes.NewForConfigOrDie(cfg)
 }
